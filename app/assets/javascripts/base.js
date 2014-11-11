74d22ffba03bd899508028c3344add6b73acafa4
Chris Gannon <cjgannon@gmail.com>
2014-11-11 21:22:13 +0000

    added tokbox and glasses

--- app/assets/javascripts/base.js
+++ app/assets/javascripts/base.js
@@ -3,23 +3,42 @@
 	var i = 0,
 		lastTime = 0,
 		vendors = ['ms', 'moz', 'webkit', 'o'];
-	
+
 	while (i < vendors.length && !window.requestAnimationFrame) {
 		window.requestAnimationFrame = window[vendors[i] + 'RequestAnimationFrame'];
 		i++;
 	}
-	
+
 	if (!window.requestAnimationFrame) {
 		window.requestAnimationFrame = function(callback, element) {
 			var currTime = new Date().getTime(),
 				timeToCall = Math.max(0, 1000 / 60 - currTime + lastTime),
 				id = setTimeout(function() { callback(currTime + timeToCall); }, timeToCall);
-			
+
 			lastTime = currTime + timeToCall;
 			return id;
 		};
 	}
 }());
+
+// Initialize API key, session, and token...
+// Think of a session as a room, and a token as the key to get in to the room
+// Sessions and tokens are generated on your server and passed down to the client
+var apiKey = "45084212";
+var sessionId = "1_MX40NTA4NDIxMn5-MTQxNTczNTc3MTA1OH5GblYvdTFHMm1TRlFOdDB6aG9laXV1MmV-fg";
+var token = "T1==cGFydG5lcl9pZD00NTA4NDIxMiZzaWc9MDMxMDVmNTE3ZTU0MDcxZDgxZDYyMmMxNDY3NmNkYjk4ZTNmMjI0MDpyb2xlPXB1Ymxpc2hlciZzZXNzaW9uX2lkPTFfTVg0ME5UQTROREl4TW41LU1UUXhOVGN6TlRjM01UQTFPSDVHYmxZdmRURkhNbTFUUmxGT2REQjZhRzlsYVhWMU1tVi1mZyZjcmVhdGVfdGltZT0xNDE1NzM3NDU5Jm5vbmNlPTAuMDMxNTIyNjExOTEyNDI2";
+
+// Initialize session, set up event listeners, and connect
+var session = OT.initSession(apiKey, sessionId);
+
+session.on("streamCreated", function(event) {
+  session.subscribe(event.stream);
+});
+
+session.connect(token, function(error) {
+  var publisher = OT.initPublisher(output, {height: 500, width: 650});
+  session.publish(publisher);
+});
 
 var App = {
 	start: function(stream) {
@@ -37,21 +56,21 @@ var App = {
 
 				App.context.translate(App.canvas.width, 0);
 				App.context.scale(-1, 1);
-			
+
 				var w = 300 / 4 * 0.8,
 					h = 270 / 4 * 0.8;
-			
+
 				App.comp = [{
 					x: (App.video.videoWidth / 4 - w) / 2,
 					y: (App.video.videoHeight / 4 - h) / 2,
-					width: w, 
+					width: w,
 					height: h,
 				}];
-			
+
 				App.drawToCanvas();
 			}, 500);
 		}, true);
-		
+
 		var domURL = window.URL || window.webkitURL;
 		App.video.src = domURL ? domURL.createObjectURL(stream) : stream;
 	},
@@ -66,7 +85,7 @@ var App = {
 	},
 	drawToCanvas: function() {
 		requestAnimationFrame(App.drawToCanvas);
-		
+
 		var video = App.video,
 			ctx = App.context,
 			backCtx = App.backContext,
@@ -77,23 +96,26 @@ var App = {
 
 		ctx.drawImage(video, 0, 0, App.canvas.width, App.canvas.height);
 		backCtx.drawImage(video, 0, 0, App.backCanvas.width, App.backCanvas.height);
-		
+
 		comp = ccv.detect_objects(App.ccv = App.ccv || {
 			canvas: App.backCanvas,
 			cascade: cascade,
 			interval: 4,
 			min_neighbors: 1
 		});
-		
+
 		if (comp.length) {
 			App.comp = comp;
 		}
-		
+
 		for (i = App.comp.length; i--; ) {
 			ctx.drawImage(App.glasses, (App.comp[i].x - w / 2) * m, (App.comp[i].y - w / 2) * m, (App.comp[i].width + w) * m, (App.comp[i].height + w) * m);
 		}
 	}
 };
+
+//GLASSES
+var glasses = "glasses.png";
 
 // MOUSTACHE ARRAY
 var moustache = [];
@@ -117,10 +139,20 @@ moustache[16] = "16.png";
 moustache[17] = "no-tash.png";
 
 App.glasses = new Image();
-App.glasses.src = "/assets/" + moustache[0];
+App.glasses.src = "/assets/" + moustache[17];
 
 function changeMoustache(value) {
   App.glasses.src = "/assets/" + moustache[value];
+}
+
+//DEAL WITH IT FUNCTIONS
+
+function dealWithIt() {
+  App.glasses.src = "/assets/" + glasses;
+}
+
+function doneWithIt() {
+  App.glasses.src = "/assets" + moustache[17];
 }
 
 //CANVAS TAGS
@@ -133,6 +165,8 @@ var context;
 var moustacheOptions = document.getElementById('options');
 
 //HTML BUTTONS
+var dealButton = document.getElementById('deal');
+var doneBustton = document.getElementById('done');
 var takeButton = document.getElementById('take');
 var saveButton = document.getElementById('save');
 var delButton = document.getElementById('delete');
@@ -149,38 +183,38 @@ var hour = timestamp.getHours();
 var min = timestamp.getMinutes();
 var yyyy = timestamp.getFullYear();
 
-var timeInSecs; 
-var ticker; 
+var timeInSecs;
+var ticker;
 
-function startTimer(secs) { 
+function startTimer(secs) {
   document.getElementById("countdownBlock").classList.add('counting');
-  timeInSecs = parseInt(secs)-1; 
-  ticker = setInterval("tick()",1000); // every second 
-} 
+  timeInSecs = parseInt(secs)-1;
+  ticker = setInterval("tick()",1000); // every second
+}
 
-function tick() { 
-  var secs = timeInSecs; 
-  if (secs>1) { 
-    timeInSecs--; 
-  } else { 
-    clearInterval(ticker); 
+function tick() {
+  var secs = timeInSecs;
+  if (secs>1) {
+    timeInSecs--;
+  } else {
+    clearInterval(ticker);
     setTimeout(function() {
       document.getElementById("countdownBlock").classList.remove('counting');
       flash.classList.add('active');
       setTimeout(function(){flash.classList.remove('active');},100);
       document.getElementById("countdownTime").innerHTML = '3';
     },100);
-  } 
-  document.getElementById("countdownTime").innerHTML = secs; 
+  }
+  document.getElementById("countdownTime").innerHTML = secs;
 }
 
 function takePhoto() {
-  
+
   photo.width = video.width;
   photo.height = video.height;
-  
+
   startTimer(4);
-  
+
   setTimeout(function(){
     context = photo.getContext('2d');
     context.drawImage(video, 0, 0, photo.width, photo.height);
@@ -193,10 +227,10 @@ function takePhoto() {
     saveButton.style.display = 'inline-block';
     anotherButton.style.display = 'inline-block';
     moustacheOptions.style.display = 'none';
-  },4000); 
+  },4000);
 }
 
-function savePhoto() {  
+function savePhoto() {
   if(dd<10){dd='0'+dd} if(mm<10){mm='0'+mm} timestamp = mm+'-'+dd+'-'+yyyy+'_'+hour+'-'+min;
   photo.toBlob(function(blob) {
       saveAs(blob, "Photo_"+timestamp+".png");
@@ -209,7 +243,7 @@ function takeAnother() {
   takeButton.style.display = 'inline-block';
   saveButton.style.display = 'none';
   anotherButton.style.display = 'none';
-  moustacheOptions.style.display = 'block';                 
+  moustacheOptions.style.display = 'block';
 }
 
 // REMOVED THE DELETE FUNCTION
@@ -225,9 +259,9 @@ App.init = function() {
 	App.context = App.canvas.getContext('2d');
 
 	App.info = document.querySelector('#info');
-	
+
 	navigator.getUserMedia_ = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
-	
+
 	try {
 		navigator.getUserMedia_({
 			video: true,
@@ -240,7 +274,7 @@ App.init = function() {
 			App.error(e);
 		}
 	}
-	
+
 	App.video.loop = App.video.muted = true;
 	App.video.load();
 };
